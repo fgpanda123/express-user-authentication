88f6cc8bfb985e946843921e94d4db166a4dc856
zuhuiwu <joggers.oldster.07@icloud.com>
2025-06-29 20:38:00 -0700

    init

--- test/check_status.sh
+++
@@ -1,2 +0,0 @@
-curl -X GET http://localhost:3000/api/protected \
--H "Content-Type: application/json"
--- test/test_login.sh
+++
@@ -1,6 +0,0 @@
-curl -X POST http://localhost:3000/api/auth/login \
-           -H "Content-Type: application/json" \
-           -d '{"email":"john@example.com","password":"Password123"}' \
-
-
-
